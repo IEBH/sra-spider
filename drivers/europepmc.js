28710887fc152b70f83069fcde8f473e240b245a
Mhodd <33404472+Mhodd@users.noreply.github.com>
2019-05-09 00:30:24 +0100

    Enabled backward and forward connected citation fetching via the factory. Both fetching methods automatically recurse via the recursive pagination utility.

--- drivers/europepmc.js
+++ drivers/europepmc.js
@@ -2,6 +2,40 @@ const axios = require('axios');
 
 const BASE_URL = 'https://www.ebi.ac.uk/europepmc/webservices/rest';
 
+const europepmcToSraFields = {
+  id: 'pmid',
+  pmid: 'pmid',
+  pmcid: 'pmcid',
+  doi: 'doi',
+  title: 'title',
+  journalTitle: 'journal',
+  pageInfo: 'pages',
+  journalVolume: 'volume',
+  issue: 'number',
+  pubType: 'type',
+  pubYear: 'date',
+  abstractText: 'abstract',
+  authorString: 'authors',
+}
+
+/**
+ * @param {Object} europepmcCitation
+ * @returns {Object}
+ */
+const parseEuropepmcCitationToSra = (europepmcCitation) => {
+  if (!europepmcCitation) return null;  
+
+  const sraCitation = {};
+
+  Object.keys(europepmcCitation)
+    .filter(field => europepmcToSraFields.hasOwnProperty(field))
+    .forEach(field => {
+      sraCitation[europepmcToSraFields[field]] = europepmcCitation[field];
+    })
+
+  return sraCitation;
+}
+
 /**
  * @returns {Object}
  */
@@ -13,13 +47,79 @@ const EuropepmcDriver = () => {
   })
 
   /**
+   * @param {string} pmid
+   * @returns {Promise<Object[]>}
+   */
+  const getBackwardsCitations = async (pmid) => {
+    const pages = await paginate(async ({ page, pageSize }) => {
+
+      const response = await client.get(`/MED/${pmid}/references`, {
+        params: {
+          page, pageSize, format: 'JSON',
+        }
+      })
+
+      const { hitCount } = response.data;
+      if (hitCount === 0) {
+        return { 
+          total: 0, data: [],
+        };      
+      }
+  
+      const { reference: europepmcCitations } = response.data.referenceList;
+      return {
+        total: hitCount,
+        data: europepmcCitations,
+      };
+
+    }, {
+      page: 1, pageSize: 1000,
+    });
+
+    return pages.reduce((citations, page) => [...citations, ...page.data.map(parseEuropepmcCitationToSra)], []);
+  }
+
+  /**
+   * @param {string} pmid
+   * @returns {Promise<Object[]>}
+   */
+  const getForwardCitations = async (pmid) => {
+    const pages = await paginate(async ({ page, pageSize }) => {
+
+      const response = await client.get(`/MED/${pmid}/citations`, {
+        params: {
+          page, pageSize, format: 'JSON',
+        }
+      })
+
+      const { hitCount } = response.data;
+      if (hitCount === 0) {
+        return { 
+          total: 0, data: [],
+        };       
+      }
+  
+      const { citation: europepmcCitations } = response.data.citationList;
+      return {
+        total: hitCount,
+        data: europepmcCitations,
+      };
+
+    }, {
+      page: 1, pageSize: 1000,
+    });
+
+    return pages.reduce((citations, page) => [...citations, ...page.data.map(parseEuropepmcCitationToSra)], []);
+  }
+
+  /**
    * @param {string} direction
    * @returns {function}
    */
   const getCitationsFactory = (direction) => {
     switch (direction) {
-      case 'backwards': return () => Promise.resolve([]);
-      case 'forwards': return () => Promise.resolve([]);
+      case 'backwards': return getBackwardsCitations;
+      case 'forwards': return getForwardCitations;
       /* If the direction is not supported,  */
       default: return () => Promise.resolve([]);
     }
--- tests/drivers/europepmc.js
+++ tests/drivers/europepmc.js
@@ -18,5 +18,55 @@ describe('getConnectedCitations', () => {
       expect(error).to.be.not.ok;
     }
   })
+  
+  it('should return a list of backward citations given a pmid.', async () => {
+    const citation = { pmid: '19826172' };
+
+    try {
+      const citations = await europepmcDriver.getConnectedCitations(citation, {
+        directions: ['backwards'],
+      });
+      expect(citations.length).to.be.at.least(56);
+    } catch (error) {
+      expect(error).to.be.not.ok;
+    }  
+  })
+
+  it('should return a list of forward citations given a pmid.', async () => {
+    const citation = { pmid: '19826172' };
+
+    try {
+      const citations = await europepmcDriver.getConnectedCitations(citation, {
+        directions: ['forwards'],
+      });
+      expect(citations.length).to.be.at.least(134);
+    } catch (error) {
+      expect(error).to.be.not.ok;
+    }
+  })
+
+  it('should return a list of connected citations using multiple directions matching of the sum of the individual directions.', async () => {
+    const citation = { pmid: '19826172' };
+
+    let numberOfConnectedCitations = 0;
+
+    try {
+      numberOfConnectedCitations += (await europepmcDriver.getConnectedCitations(citation, {
+        directions: ['backwards'],
+      })).length;
+  
+      numberOfConnectedCitations += (await europepmcDriver.getConnectedCitations(citation, {
+        directions: ['forwards'],
+      })).length;
+  
+      const citations = await europepmcDriver.getConnectedCitations(citation, {
+        directions: ['backwards', 'forwards'],
+      });
+
+      expect(citations.length).to.equal(numberOfConnectedCitations);
+    } catch (error) {
+      expect(error).to.be.not.ok;
+    }
+  })
 
 })
